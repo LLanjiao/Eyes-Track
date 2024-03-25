2706acd2c80786fe6d9099cd161672bea137ef42
wyqqqqq <1961701865@qq.com>
2024-03-25 10:09:24 +0800

    main

--- mainnn.py
+++ mainnn.py
@@ -3,16 +3,19 @@ import cv2
 
 from PyQt6.QtWidgets import QApplication
 
-from frame_sources.pic import pic
+from frame_sources.image import image
 from gui.application_window import Window
+
+from settings import settings
 
 
 if __name__ == "__main__":
+    print(settings.CASES_FILE_PATH)
     app = QApplication(sys.argv)
-    p = pic('resources/face.png')
-    frame = p.start()
+    # p = image('resources/face.png')
+    # frame = p.start()
     # frame = cv2.imread('../resources/face.png')
-    window = Window(frame)
+    window = Window()
     window.setWindowTitle("Eye Tracking")
     window.show()
     sys.exit(app.exec())
