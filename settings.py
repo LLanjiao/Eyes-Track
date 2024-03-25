03853369310718bb7bb954ab16b523a950ed2ec4
wyqqqqq <1961701865@qq.com>
2024-03-25 10:08:56 +0800

    设置变量

--- settings.py
+++ settings.py
@@ -8,10 +8,14 @@ e = environ.get
 
 @dataclass
 class Settings:
-
     BASE_DIR = Path(os.path.split(os.path.abspath(__file__))[0])
     ASSETS: Path = BASE_DIR / "gui" / "assets"
 
     GUI_FILE_PATH: Path = e("GUI_FILE_PATH", ASSETS / "Eyes-Track.ui")
+
+    CASES_FILE_PATH = e("CASES_FILE_PATH", BASE_DIR / "resources")
+
+    REFRESH_PERIOD: int = e("CAMERA_REFRESH_PERIOD", 33)
+
 
 settings = Settings()
