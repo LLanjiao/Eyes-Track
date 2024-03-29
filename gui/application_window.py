8b34909d4e6a9c39dbcb1c2cd40d61a584fe7a6f
wyqqqqq <1961701865@qq.com>
2024-03-29 11:33:05 +0800

    GUI管理

--- gui/application_window.py
+++ gui/application_window.py
@@ -1,12 +1,14 @@
 import warnings
 import warnings
 
+import cv2
 import numpy
 from PyQt6.QtCore import QTimer
 from PyQt6.QtGui import QImage, QPixmap
-from PyQt6.QtWidgets import QWidget, QPushButton, QLabel, QFileDialog, QSlider
+from PyQt6.QtWidgets import QWidget, QPushButton, QLabel, QFileDialog, QSlider, QComboBox
 from PyQt6.uic import loadUi
 
+from frame_sources.camera import camera
 from frame_sources.image import image
 from frame_sources.video import video
 from function.tracking import eyes_tracking
@@ -16,10 +18,11 @@ warnings.filterwarnings("ignore", category=DeprecationWarning)
 
 
 class Window(QWidget):
-    startCamera: QPushButton
+    camera: QPushButton
     openFile: QPushButton
     stop: QPushButton
     threshSlider: QSlider
+    cameraChoose: QComboBox
 
     def __init__(self):
         super(Window, self).__init__()
@@ -27,16 +30,32 @@ class Window(QWidget):
 
         self.filePath = None
         self.fileType = None
+        self.cameraType = None
         self.frame = None
         self.timer = None
         self.frameSources = None
+        self.isCameraOpening = False
+        self.isPlaying = False
         self.thresh = 50
         self.tracking = eyes_tracking()
 
-        self.startCamera.clicked.connect(self.play)
+        self.camera.clicked.connect(self.cameraController)
         self.openFile.clicked.connect(self.fileChoose)
         self.stop.clicked.connect(self.stopPlay)
         self.threshSlider.valueChanged.connect(self.threshChange)
+
+    def cameraController(self):
+        if self.isCameraOpening:
+            self.isCameraOpening = False
+            self.cameraType = None
+            self.camera.setText("openCamera")
+            self.stopPlay()
+        else:
+            self.isCameraOpening = True
+            self.fileType = "camera"
+            self.cameraType = self.cameraChoose.currentText()
+            self.camera.setText("closeCamera")
+            self.play()
 
     def fileChoose(self):
         """
@@ -57,16 +76,21 @@ class Window(QWidget):
             self.fileType = "image"
         elif response[1] == "video(*.mov *.mp4)":
             self.fileType = "video"
+        if self.fileType is not None:
+            self.play()
 
     def play(self):
         if self.fileType == "image":
             self.frameSources = image(self.filePath)
         elif self.fileType == "video":
             self.frameSources = video(self.filePath)
+        elif self.fileType == "camera":
+            self.frameSources = camera(self.cameraType)
         self.frameSources.start()
         self.timer = QTimer()
         self.timer.timeout.connect(self.nextFrame)
         self.timer.start(settings.REFRESH_PERIOD)
+        self.isPlaying = True
         # self.nextFrame()
 
     def nextFrame(self):
@@ -74,23 +98,48 @@ class Window(QWidget):
         if not ret:
             self.timer.stop()
         else:
-            frame, eyesImage, redWeight, histogram, binary, rightEye = self.tracking.track(self.frame, self.thresh)
-            self.display_image(self.opencv_to_qt(frame))
-            self.display_image(self.opencv_to_qt(eyesImage), window="eyesImage")
-            self.display_image(self.opencv_to_qt(redWeight), window="redWeight")
-            self.display_image(self.opencv_to_qt(histogram), window="histogram")
-            self.display_image(self.opencv_to_qt(binary), window="binary")
-            self.display_image(self.opencv_to_qt(rightEye), window="rightEye")
+            haveFace, frame, eyeImage_left, redWeight_left, histogram_left, binary_left, trackingEye_left, \
+            eyeImage_right, redWeight_right, histogram_right, binary_right, trackingEye_right \
+                = self.tracking.track(self.frame, self.thresh)
+            if haveFace:
+                self.display_image(self.opencv_to_qt(frame))
+
+                self.display_image(self.opencv_to_qt(eyeImage_left), window="eyeImage_left")
+                self.display_image(self.opencv_to_qt(redWeight_left), window="redWeight_left")
+                self.display_image(self.opencv_to_qt(histogram_left), window="histogram_left")
+                self.display_image(self.opencv_to_qt(binary_left), window="binary_left")
+                self.display_image(self.opencv_to_qt(trackingEye_left), window="tracking_left")
+
+                self.display_image(self.opencv_to_qt(eyeImage_right), window="eyeImage_right")
+                self.display_image(self.opencv_to_qt(redWeight_right), window="redWeight_right")
+                self.display_image(self.opencv_to_qt(histogram_right), window="histogram_right")
+                self.display_image(self.opencv_to_qt(binary_right), window="binary_right")
+                self.display_image(self.opencv_to_qt(trackingEye_right), window="tracking_right")
+            else:
+                self.display_image(self.opencv_to_qt(frame))
 
     def stopPlay(self):
-        self.frameSources.stop()
-        self.timer.stop()
-        self.findLabelbyName("faceFrame").setText("faceFrame")
-        self.findLabelbyName("eyesImage").setText("eyesImage")
-        self.findLabelbyName("redWeight").setText("redWeight")
-        self.findLabelbyName("histogram").setText("histogram")
-        self.findLabelbyName("binary").setText("binary")
-        self.findLabelbyName("rightEye").setText("rightEye")
+        if self.isPlaying:
+            self.frameSources.stop()
+            self.timer.stop()
+            self.findLabelbyName("faceFrame").setText("faceFrame")
+            self.findLabelbyName("eyeImage_left").setText("eyeImage_left")
+            self.findLabelbyName("redWeight_left").setText("redWeight_left")
+            self.findLabelbyName("histogram_left").setText("histogram_left")
+            self.findLabelbyName("binary_left").setText("binary_left")
+            self.findLabelbyName("tracking_left").setText("tracking_left")
+
+            self.findLabelbyName("eyeImage_right").setText("eyesImage_right")
+            self.findLabelbyName("redWeight_right").setText("redWeight_right")
+            self.findLabelbyName("histogram_right").setText("histogram_right")
+            self.findLabelbyName("binary_right").setText("binary_right")
+            self.findLabelbyName("tracking_right").setText("tracking_right")
+
+            self.filePath = None
+            self.fileType = None
+            self.frame = None
+            self.frameSources = None
+            self.isPlaying = False
 
     @staticmethod
     def opencv_to_qt(img) -> QImage:
