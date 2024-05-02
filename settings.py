b807304c054ff380c1878cd4cff59aff746f5410
wyqqqqq <1961701865@qq.com>
2024-05-02 12:30:43 +0800

    添加了视线定位ui路径

--- settings.py
+++ settings.py
@@ -11,12 +11,14 @@ class Settings:
     BASE_DIR = Path(os.path.split(os.path.abspath(__file__))[0])
     ASSETS: Path = BASE_DIR / "gui" / "assets"
 
-    GUI_FILE_PATH: Path = e("GUI_FILE_PATH", ASSETS / "Eyes-Track.ui")
+    MAINWINDOW_GUI_FILE_PATH: Path = e("MAINWINDOW_GUI_FILE_PATH", ASSETS / "Eyes-Track.ui")
+    LOCATEWINDOW_GUI_FILE_PATH: Path = e("LOCATEWINDOW_GUI_FILE_PATH", ASSETS / "locate.ui")
 
     CASES_FILE_PATH = e("CASES_FILE_PATH", BASE_DIR / "resources")
     PREDICTOR_PATH = e("PREDICTOR_PATH", BASE_DIR / "predictor" / "shape_predictor_68_face_landmarks.dat")
 
     REFRESH_PERIOD: int = e("CAMERA_REFRESH_PERIOD", 33)
+    REFRESH_PERIOD_1S: int = e("REFRESH_PERIOD_1S", 1000)
 
 
 settings = Settings()
